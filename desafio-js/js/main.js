4f574a7bc7b3617c9e8dce9ce9fa99aa5dd7931d
Felipe De Andrade <felipe.deam98@gmail.com>
2024-03-18 20:17:41 -0300

    Transformação seções Menu, Header e Cards

--- desafio-js/js/main.js
+++ desafio-js/js/main.js
@@ -27,13 +27,13 @@ const newOrder = [1, 3, 2, 0];
 listCards.forEach((card, index) => {
   card.parentNode.style.order = newOrder[index];
 
-  if(index === 0) {
+  if (index === 0) {
     const btn = card.querySelector('.btn');
     changeColor(btn, bgBtn, bgBtnHover);
   }
 })
 
-function changeColor (element, color, colorHover) {
+function changeColor(element, color, colorHover) {
   element.style.background = color;
   element.style.borderColor = color;
 
@@ -41,9 +41,33 @@ function changeColor (element, color, colorHover) {
     element.style.background = colorHover;
     element.style.borderColor = colorHover;
   })
-  
-  element.addEventListener('mouseleave', function() {
+
+  element.addEventListener('mouseleave', function () {
     element.style.background = color;
     element.style.borderColor = color;
   });
 }
+
+const list = document.querySelector('.list-group');
+
+const item = document.createElement('li');
+item.classList.add('list-group-item');
+
+const newItensText = [
+  'Quarto Item',
+  'Quinto Item',
+]
+
+newItensText.forEach(text => {
+  const item = document.createElement('li');
+  item.classList.add('list-group-item');
+  item.textContent = text
+  list.appendChild(item);
+})
+
+const listItens = list.querySelectorAll('li');
+
+listItens.forEach(item => {
+  item.classList.contains('active') ? item.classList.remove('active') : ''
+  item.textContent === 'Quarto Item' ? item.classList.add('active') : ''
+})

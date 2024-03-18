344add71363509395e4588e248ae2dc663117257
Felipe De Andrade <felipe.deam98@gmail.com>
2024-03-18 19:23:45 -0300

    Transformação seções Menu, Header e Cards

---
+++ desafio-js/js/main.js
@@ -0,0 +1,49 @@
+
+const bgBtn = '#27a844'
+const bgBtnHover = '#22913b'
+
+const menuContainer = document.querySelector('.btn-group-vertical');
+const btnsMenu = menuContainer.querySelectorAll('.btn');
+
+menuContainer.style.flexDirection = 'row-reverse';
+menuContainer.style.gap = '.25rem';
+
+btnsMenu.forEach(botao => {
+  botao.style.borderRadius = '.25rem';
+  botao.style.width = 'auto';
+});
+
+const headerContainer = document.querySelector('.jumbotron');
+headerContainer.style.textAlign = 'end';
+headerContainer.style.background = '#6b757e';
+headerContainer.style.color = '#fff';
+
+const btnHeader = headerContainer.querySelector('.btn');
+changeColor(btnHeader, bgBtn, bgBtnHover);
+
+const listCards = document.querySelectorAll('.card');
+const newOrder = [1, 3, 2, 0];
+
+listCards.forEach((card, index) => {
+  card.parentNode.style.order = newOrder[index];
+
+  if(index === 0) {
+    const btn = card.querySelector('.btn');
+    changeColor(btn, bgBtn, bgBtnHover);
+  }
+})
+
+function changeColor (element, color, colorHover) {
+  element.style.background = color;
+  element.style.borderColor = color;
+
+  element.addEventListener('mouseenter', () => {
+    element.style.background = colorHover;
+    element.style.borderColor = colorHover;
+  })
+  
+  element.addEventListener('mouseleave', function() {
+    element.style.background = color;
+    element.style.borderColor = color;
+  });
+}
